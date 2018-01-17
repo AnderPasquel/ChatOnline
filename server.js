e171926fe386bbc2125b7a55139ae7c35709db4d
AnderPasquel <anderpasquelrosales@gmail.com>
2018-01-16 18:05:45 -0600

    Add emotes
    Add emotes window

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var server = require('http').Server(app);
 var port = process.env.PORT || 3000
 var io = require('socket.io')(server);
 
+//REQUIRE EMOJIS LIST MODULE
+var emojis = require("emojis-list")
+
 app.get('/test', function (req, res) {
     res.status(200).send('TestServer');
 });
@@ -11,29 +14,34 @@ app.get('/test', function (req, res) {
 app.use(express.static('public'));
 
 server.listen(port, function () {
-  console.log('La aplicacion esta correinde en el puerto ' + port);
+    console.log('La aplicacion esta correinde en el puerto ' + port);
 });
 
 var messages = [{
-    id:1,
-    text:'Bienvenido',
-    nickname:'CHAT--BOT'
+    id: 1,
+    text: 'Bienvenido',
+    nickname: 'Herraminta de chat'
 }];
 
 io.on('connection', function (socket) {
     console.log("El dispositivo con ip:" + socket.handshake.address + "se ha conectado");
-
     socket.emit('messages', messages);
-
-    socket.on('add-Message', function(data){
+    socket.on('add-Message', function (data) {
         messages.push(data);
-        io.sockets.emit('messages',messages);
+        io.sockets.emit('messages', messages);
     });
 });
 
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+/*//LOG THE TOTAL LENGTH OF OUR ARRAY
+console.log("There are " + emojis.length + " emojis");
+//LOG THE FIRST THIRTY EMOJIS
+for (var i = 0; i < 800; i++) {
+    console.log(i + 1 + ". " + emojis[i]);
+}*/
